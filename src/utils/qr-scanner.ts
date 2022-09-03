8c68fb48f612c8b60db5ff9c57b0e0a4d47fc0c8
netd <netd777@gmail.com>
2022-09-02 22:59:25 -0300

    mend

--- src/utils/qr-scanner.ts
+++ src/utils/qr-scanner.ts
@@ -1,7 +1,20 @@
 import QrScanner from 'qr-scanner';
 
 export const initializeScanner = (videoEl: HTMLVideoElement, onScan: (result) => void | undefined) => {
-  return new QrScanner(videoEl, result => onScan?.(result), {
+  const scanner = new QrScanner(videoEl, result => onScan?.(result), {
     maxScansPerSecond: 20,
   });
+
+  const parentRect = videoEl.parentElement?.getBoundingClientRect() as {
+    width: number;
+    height: number;
+    top: number;
+    bottom: number;
+    left: number;
+    right: number;
+  };
+  const width = (parentRect.width > parentRect.height) ? parentRect.height : parentRect.width;
+  videoEl.setAttribute('style', `width: ${width.toFixed}px`);
+
+  return scanner;
 }
